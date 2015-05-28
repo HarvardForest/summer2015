78551fe36b5ff85f65e758117cfdb523df4cd515
Nathan <nthnjustice@gmail.com>
2015-05-28 18:39:31 -0400

    Added edge weight labeling

--- Rgraphviz/practice/oysterGraph.R
+++ Rgraphviz/practice/oysterGraph.R
@@ -25,11 +25,28 @@ graphInfo <- agopen(g1, name="g1Properties") # Object used to store graph proper
 
 edges <- which(F > 0, arr.in=TRUE) # Find all edges
 
+# Add all edges to their appropriate Nodes
 for (i in 1:length(edges[,1])) {
-  x[i] <- F[edges[i], edges[i,2]]
-  print(edges[i])
-  g1 <- addEdge (rownames(F)[edges[i,1], colnames(F)edges[i,2]], g1, F[edges[i], edges[i,2]])
+  g1 <- addEdge (rownames(F)[edges[i,1]], colnames(F)[edges[i,2]], g1, F[edges[i], edges[i,2]])
 }
 
-plot(g1)
+## Add edge labels (by weight) ##
+
+eAttrs <- list()
+attrs <- list(node=list(shape="ellipse", fixedsize=FALSE))
+
+# Get edge weights, unlist them, provide in vector format
+ew <- as.character(unlist(edgeWeights(g1)))
+
+# Determine edges to remove - necessary if recipEdges=TRUE (Default)
+ew <- ew[setdiff(seq(along=ew), removedEdges(g1))]
+
+# Get the set of edge names
+names(ew) <- edgeNames(g1)
+
+# Set global attributes
+eAttrs$label <- ew
+attrs$edge$fontsize <- 27
+
+plot(g1, recipEdges="distinct", edgeAttrs=eAttrs, attrs=attrs)
 
