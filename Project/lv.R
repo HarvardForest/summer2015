6f4a47b3a76262d6b5096536d65fcf2f94b1ba29
Nathan <nthnjustice@gmail.com>
2015-06-01 22:42:46 -0400

    Updated LotV predator-prey model

--- Project/lv.R
+++ Project/lv.R
@@ -1,25 +1,36 @@
+# Load dependencies
 library(deSolve)
 
-lotVmodel <-function(time, initState, params){
-  with(as.list(c(initState, params)),{
+lotVpredPrey <- function(time, initState, params){
+  # Function for ordinary differential equations (ODE)
+  lotVPPeqs <-function(time, initState, params){
+    with(as.list(c(initState, params)),{
 
-    # Lotka-Volterra equations
-    dx <- (alpha * prey) - (beta * prey * predator)
-    dy <- (gamma * prey * predator) - (delta * predator)
+      # Lotka-Volterra predator-prey model
+      dx <- (alpha * prey) - (beta * prey * predator)
+      dy <- (gamma * prey * predator) - (delta * predator)
 
-    # alpha = the growth rate of prey
-    # beta = the rate at which predators kill prey
-    # delta = the death rate of predators
-    # gamma = the rate at which predators increase by consuming prey
+      # alpha = the growth rate of prey
+      # beta = the rate at which predators kill prey
+      # delta = the death rate of predators
+      # gamma = the rate at which predators increase by consuming prey
 
-    list(c(dx, dy))
-  })
+      list(c(dx, dy))
+    })
+  }
+
+  # deSolve method to solve initial value problems (IVP)
+  output <- ode(y=initState, times=time, func=lotVPPeqs, parms=params)
+  # Plot matrix
+  matplot(output[,-1], type="l", xlab="Time", ylab="Population")
+  legend("topleft", c("Prey", "Predator"), lty=c(1, 2), col=c(1, 2),
+         bty="n")
 }
 
-params <- c(alpha=1.15, beta=0.01, delta=0.4, gamma=0.001)
+# Test-values
+params <- c(alpha=1.5, beta=0.02, delta=0.4, gamma=0.01)
 initState <- c(prey=500, predator=10)
-time <- seq(0, 100, by=.1)
+time <- seq(0, 100, by=1)
 
-output <- ode(y=initState, times=time, func=lotVmodel, parms=params)
-matplot(output[,-1], type="l", xlab="Time", ylab="Population")
-legend("topright", c("Prey", "Predator"), lty = c(1, 2), col = c(1, 2), box.lwd = 0)
+# Function-call
+lotVpredPrey(time, initState, params)
