01aada3729a6dbdf57c5cd996d405b6451c29617
Nathan <nthnjustice@gmail.com>
2015-06-08 10:15:42 -0400

    PPsim initial conditions set

--- pitcherStack.R
+++ pitcherStack.R
@@ -1,4 +1,4 @@
-#### Pitcher plant O2 simulation - stack-based
+#### Pitcher plant O2 simulation - stack algorithm
 ### Nathan Justice
 ## Last edited: 08June2015
 
@@ -7,11 +7,13 @@
 # 18:00 sunset = 1080
 
 ## Variables ##
+
 minute <- 0 # t/time variable
 days <- 10 # total number of days
 totalMinutes <- days*1440 # total number of minutes
 k <- 1 # carrying capacity
 food <- FALSE # presence of food
+feedingTime <- 720 # time at which food is added
 aMax <- 10 # maximum value of augmentation
 aMin <- 1 # minimum value of augmentation
 beta <- 0.0005 # constant
@@ -25,7 +27,8 @@ B <- vector(mode="numeric") # biological o2 demand
 n <- vector(mode="numeric") # amount of nutrients
 w <- vector(mode="numeric") # amount of food
 
-## Functions
+## Functions ##
+
 light <- function(days){
   out <- sin(2*pi*(1:720)/1440)
   out[out < 0] <- 0
@@ -39,7 +42,27 @@ PAR <- function(days, rise=6, set=18){
   rep(out, days)
 }
 
-## Initialization
+## Initialization ##
 
-P <- light(days) * PAR(days=days)
+# simulate photosynthesis as fixed values
+P <- light(days)*PAR(days=days)
 
+# initial nutrient value
+n <- 0
+
+# initial augmentation value
+a <- (aMax-aMin)/(1+exp(s*n*d))
+
+# initial amount of food
+w <- 0
+
+# initial biological o2 demand
+B <- 0/(k+0)
+
+# o2 at minute=0, P=0 b/c unable to index at minute=0
+x <- (a*0)-B
+
+# simulate until food is first added (feedingTime=720)
+for(i in 1:length(feedingTime)){
+
+}
