4f13edb28653319d2eb88af08cbff645828ffd25
Nathan <nthnjustice@gmail.com>
2015-06-08 16:52:04 -0400

    PPsim runs over multiple days

--- pitcherStack.R
+++ pitcherStack.R
@@ -9,7 +9,6 @@
 ## Variables ##
 
 days <- 10 # total number of days
-totalMinutes <- days*1440 # total number of minutes
 k <- 1 # carrying capacity
 food <- FALSE # presence of food
 feedingTime <- 720 # time at which food is added
@@ -20,6 +19,7 @@ beta <- 0.0005 # constant
 s <- 10 # constant
 d <- 0.5 # constant
 c <- 100 # constant
+Bscaler <- 10 # scales biological oxygen demand values
 minute <- vector(mode="numeric") # t/time variable
 x <- vector(mode="numeric") # amount of o2
 a <- vector(mode="numeric") # augmentation function
@@ -52,7 +52,7 @@ P <- light(days)*PAR(days=days)
 n <- 0
 
 # initial augmentation value
-a <- (aMax-aMin)/(1+exp(s*n*d))
+a <- ((aMax-aMin)/(1+exp((-s*n)-d)))+aMin
 
 # initial biological o2 demand
 B <- 0/(k+0)
@@ -64,7 +64,7 @@ x <- (a*0)-B
 # loop runs until feedingTime-2 b/c food is added AT minute=720
 for(i in 1:(feedingTime-2)){
   # augmentation function - default value
-  a <- c(a, (aMax-aMin)/(1+exp(-s*n[i]*d)))
+  a <- c(a, ((aMax-aMin)/(1+exp((-s*n[i])-d)))+aMin)
 
   # biological oxygen demand - default value (no food = no microbes)
   B <- c(B, 0/(k+0))
@@ -88,29 +88,32 @@ minute <- c(minute, length(minute)+1)
 # adjust amount of food
 w <- c(w, w[length(w)])
 
-# add food
-food <- TRUE
-w <- c(w, foodWeight)
+for(k in 1:days){
+  # add food
+  food <- TRUE
+  w <- c(w, foodWeight)
 
-# run simulation for a full day
-for(j in 1:1439){
-  # adjust minute
-  minute <- c(minute, length(minute)+1)
+  # run simulation for a full day
+  for(j in 1:1439){
+    # adjust minute
+    minute <- c(minute, length(minute)+1)
 
-  # adjust biological o2 demand
-  B <- c(B, w[length(minute)]/(k+w[length(minute)]))
+    # adjust biological o2 demand
+    B <- c(B, (w[length(minute)]/(k+w[length(minute)]))*Bscaler)
 
-  # adjust amount of nutrients
-  n <- c(n, (w[length(minute)]*x[length(minute)-1])/c)
+    # adjust amount of nutrients
+    n <- c(n, (w[length(minute)]*x[length(minute)-1])/c)
 
-  # adjust augmentation value
-  a <- c(a, ((aMax-aMin)/(1+exp(-(s*n[length(minute)]-d))+aMin)))
+    # adjust augmentation value
+    a <- c(a, ((aMax-aMin)/(1+exp((-s*n[length(minute)])-d)))+aMin)
 
-  x <- c(x, (a[length(minute)]*P[length(minute)])-B[length(minute)])
+    # adjust o2 amount
+    x <- c(x, (a[length(minute)]*P[length(minute)])-B[length(minute)])
 
-  if(j < 1439){
-    # adjust amount of food
-    w <- c(w, w[length(minute)]*exp(-beta*length(minute)))
+    if(j < 1439){
+      # adjust amount of food
+      w <- c(w, w[length(minute)]*exp(-beta*(length(minute)+1)))
+    }
   }
 }
 
